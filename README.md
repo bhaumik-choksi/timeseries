6e452663011473253bf4aea4ec9f8aeaf8d3d828
Bhaumik <bhaumik@bhaumik.com>
2019-03-11 14:16:42 -0700

    Readme update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # timeseries
-Time-series data prediction using an ARIMA model 
+Interactive web-based time-series data prediction using an ARIMA model.
+- [Hosted using Heroku](https://mytimeseries.herokuapp.com/)
+- Back-end in Python using Flask
+- Link to [dataset](https://datamarket.com/data/set/22me/sales-of-new-one-family-houses-usa-from-jan-1987-through-nov-1995#!ds=22me&display=line)
--- app.py
+++ app.py
@@ -19,20 +19,19 @@ def train():
         result = request.form
         pred_size = int(result['predsize'])
         step_size = pred_size
-        pred_size = 1
         raw_data = pd.read_csv('static/house-sales.csv')
         timeseries = np.asarray(raw_data['sales'])
         buffer = timeseries.tolist()
         pred = []
-        for t in range(pred_size):
-            print(t)
-            model = ARIMA(buffer, order=(8, 1, 0))
-            model_fit = model.fit(disp=0)
-            output = model_fit.forecast(steps=step_size)
-            print(output)
-            y = output[0]
-            pred.extend(y)
-            buffer.extend(y)
+
+        # Training an ARIMA model
+        model = ARIMA(buffer, order=(8, 1, 0))
+        model_fit = model.fit(disp=0)
+        output = model_fit.forecast(steps=step_size)
+        print(output)
+        y = output[0]
+        pred.extend(y)
+        buffer.extend(y)
 
         plot = figure(plot_width=900, plot_height=500)
         plot.multi_line([list(range(len(timeseries))),
@@ -45,18 +44,6 @@ def train():
         return render_template('result.html', div=div, script=script)
     else:
         return "<h1>Direct access not allowed</h1>"
-
-
-def generate_input_and_output(timeseries, stepsize=3):
-    x = []
-    y = []
-    i = 0
-    while i + stepsize < len(timeseries):
-        x.append(timeseries[i: i + stepsize])
-        y.append(timeseries[i + stepsize])
-        i += 1
-
-    return np.asarray(x), np.asarray(y)
 
 
 if __name__ == "__main__":
